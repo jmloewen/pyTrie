ae86326cd305a6a793c075a731a451cae1fc8574
jmloewen <jonoloewen@gmail.com>
2018-07-28 12:02:38 -0700

    added functionality to transform trie back into dictionary form.  Needs testing.

--- TrieNode.py
+++ TrieNode.py
@@ -1,42 +1,35 @@
 class TrieNode(object):
     #A=0, B=1, .... Z = 25.
-    def __init__(self, letter, isDict=True, parent=None):
+    def __init__(self, letter, parent=None):
         self.letter = letter
-
-        if isDict:
-            self.arr = {}
-        else:
-            self.arr = []
-
+        self.arr = {}
         self.payload = None
         self.isEnd = False
         self.parent = parent
 
-    #This takes in a dictionary and appends its KVP to this Trie.
+    # Add a dictionary to the Trie, recursively.
     def addDict(self, d):
-        #In this context, key is key, d[key] is payload.
-        #For every key in this dictionary, I add its key to the trie node, and the value as the end payload
         for key in d.keys():
             if type(d[key]) is dict:
-                subTrie = TrieNode("*", self)
+                subTrie = TrieNode("*")
                 subTrie.addDict(d[key])
-                #self.addDict(d[key])
                 self.addWord(key, subTrie)
             #No handling for dictionaries in arrays, currently.
             else:
                 self.addWord(key, d[key])
 
-    #Word will be sent in via Text.  Letter on the node should be it's whole letter self.
+    # Add a key value pair or a word to the Trie.
     def addWord(self, word, payload=None):
         node = self
         word = word.upper()
-        #Traverse through the word until all letters are nodes.
+
+        #Traverse through the word until all letters are nodes
         for letter in word:
             letterVal = ord(letter) - 65
 
+            #If node is in, traverse.  Else, add the node.
             if node.arr.get(letterVal, None):
                 node = node.arr[letterVal]
-            #Otherwise, we have to make a new node.
             else:
                 newNode = TrieNode(letter, node)
                 node.arr[letterVal] = newNode
@@ -47,7 +40,7 @@ class TrieNode(object):
         node.payload = payload
         node.isEnd = True
 
-    #removeNode
+    #Remove this node from the Trie, don't remove *
     def removeNode(self):
         if self.parent:
             val = ord(self.letter) - 65
@@ -56,12 +49,13 @@ class TrieNode(object):
         self.arr = {}
         self.isEnd = False
 
+    #Remove the payload of this node from Trie without removing the node itself
     def cleanNode(self):
         self.isEnd = False
         self.payload = None
 
-    #want to check from the last letter up whether or not we should remove this letter
-    #if any letter down the chain isn't removed, we don't want to remove the preceding one.
+
+    #Remove a given word from the Trie
     def removeWord(self, word):
         node = self
         word = word.upper()
@@ -70,19 +64,15 @@ class TrieNode(object):
 
         #Traverse down to the last letter, then walk back up to the parent.
         while node and i < len(word):
-            #print("Letter:", node.letter)
             node = node.arr.get(ord(word[i]) - 65)
             i += 1
-        #print("Letter:", node.letter)
 
         if node:#If not node, we were given an invalid word to remove
             #There must be a more pythonic way to do this
-            if len(node.arr.keys()) > 0:
+            if len(node.arr) > 0:
                 node.cleanNode()
             else:
-                while node and len(node.arr.keys()) == 0:
-                    #print("Node letter:", node.letter)
-                    #print("Node Parent: ", node.parent)
+                while len(node.arr) == 0:
                     node.removeNode()
                     node = node.parent
                     removed = True
@@ -102,44 +92,104 @@ class TrieNode(object):
             return node.payload
         return None
 
+    def buildWordFromEnd(self, partial=""):
+        if self.parent:
+            partial = self.letter + partial
+            return self.parent.buildWordFromEnd(partial)
+        else:
+            return partial
+
+    #return the trie structure from this point as a python dictionary.
+    #TODO: O(2N).  Can be improved.
+    def trieToDict(self, newDict={}):
+        node = self
+        #For all keys at this level
+        for child in node.arr.keys():
+            cur = node.arr[child]
+            #If there's a word ending here, build a dictionary value.
+            if cur.isEnd:
+                if type(cur.payload) is TrieNode:
+                    payloadDict = cur.payload.trieToDict()
+                    newDict[cur.buildWordFromEnd()] = payloadDict
+                else:
+                    newDict[cur.buildWordFromEnd()] = cur.payload
+
+            cur.trieToDict(newDict)
+        return newDict
+        #Do O(2N) solution first
+        #Traverse down to each isEnd, ascend back up
+
+
+            #For every key on this level, we have some preceding string that is identical to the
+                #other keys on this level
+            #for every key:
+                #If it would end the string
+                    #If it has no children, we append the string to our list
+                    #If it has children, we append the string to our list and make a copy of it
+                    #This copy continues to be built downwards
+                #else it would not end the string.
+                    #a copy of the string must exist to this point for every child node
+                    #make copies if necessary
+        #keys.append(key)
+
+
+'''
     #A work in progress.  Print the keys in this trie, top down.
+    #Currently does not print each word, prints all letters in each trie branch.
     def printWordsInTrie(self):
+        letters = []
         for indexNode in self.arr.keys():
             child = self.arr[indexNode]
             if child:
                 if not child.isEnd:
-                    print(child.letter, end="")
-                    child.printWordsInTrie()
+                    for cl in child.printWordsInTrie():
+                        letters.append(child.letter + cl)
                 else:
-                    print(self.arr[indexNode].letter)
+                    letters.append(child.letter)
                     if type(child.payload) is TrieNode:
-                        child.printWordsInTrie()
+                        #print(child.payload)
+                        #letters.append(child.payload.printWordsInTrie())
+                        pass
                     else:
-                        print(child.payload)
+                        letters.append(child.payload)
             else:
                 print("We shouldn't get here.  indexNode: ", indexNode)
-
+        return letters
+'''
 #Need some init function, why not main?
 if __name__ == "__main__":
     node = TrieNode("*")
     node.addWord("albatross", "birdy")
     node.addWord("Alberta", "province")
     node.addWord("alpaca", "llama")
-    node.addWord("antigua", "barbuda")
-    #node.addWord("alibaba")
+    node.addWord("abc", "barbuda")
+    node.addWord("alibaba")
+    node.addWord("babi", "nani")
     #printWordsInTrie(node)
     #print(node.findPayload("antigua"))
 
     #print(findPayload(node, "albatross"))
 
 
-    someDict = {"abc":{"abe":"123"}, "abe":"456"}
+    someDict = {"abc":{"ard":[1,2,3,4], "babby":"boy"}, "ard":"456"}
     node.addDict(someDict)
     #printWordsInTrie(dictTrie)
-    innerTrie = node.findPayload("abc")
-    node.removeWord("alpaca")
 
-    node.printWordsInTrie()
+    #innerTrie = node.findPayload("abc")
+
+    #tempNode = node.arr[0].arr[1].arr[2]
+
+    #print(tempNode.buildWordFromEnd())
+
+    trieDict = node.findPayload("abc")
+    print(trieDict.trieToDict())
+    print(node.trieToDict())
+    #print(trieDict["ABC"]["ARD"])
+    #print(node.trieToDict())
+    #print(innerTrie.findPayload("ard"))
+
+    #print(node.printWordsInTrie())
+
     #print(node.findPayload("antigua"))
     #print(node.findPayload("abe"))
 
